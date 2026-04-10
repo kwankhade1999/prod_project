7db079d260ad1ade8701d53456958f2b6d503707
Kunal Wankhade <kwakhade1999gmail.com>
2026-04-10 20:32:41 +0000

    ka

--- Infrastructure-main/Infrastructure-main/1-network/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "quantamvector-infra-statefile-backup"
-    key            = "quantamvector/1-network/terraform.tfstate"
-    region         = "ap-northeast-1"
-    dynamodb_table = "quantamvector-terraform-locks"
-    encrypt        = true
-  }
-}
--- Infrastructure-main/Infrastructure-main/2-eks/data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "network" {
-  backend = "s3"
-
-  config = {
-    bucket = "quantamvector-infra-statefile-backup"
-    key    = "quantamvector/1-network/terraform.tfstate"
-    region = "ap-northeast-1"
-  }
-}
